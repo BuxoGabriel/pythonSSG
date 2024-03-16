576aad1af563006e1b9b43f4ef4403df893ba44a
Gabriel Buxo <gabrielbuxo1@gmail.com>
2024-03-16 03:49:13 -0400

    implemented html block_type from markdown block function and tested

--- src/markdown.py
+++ src/markdown.py
@@ -1,3 +1,10 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
 def markdown_to_blocks(markdown: str):
     blocks = []
     block = ""
@@ -17,3 +24,40 @@ def markdown_to_blocks(markdown: str):
     if(block !=""):
         blocks.append(block)
     return blocks
+
+def block_to_block_type(block: str):
+    block = block.strip()
+    # Check if ends match code block
+    if block.startswith("```") and block.endswith("```"):
+        return block_type_code
+    # Check if header
+    header_level = 0
+    for char in block:
+        if char == "#":
+            header_level += 1
+        else:
+            break
+    if header_level != 0:
+        if header_level > 6:
+            return block_type_paragraph
+        return f"{block_type_heading}_{header_level}"
+
+    # check if every line matches a pattern
+    lines = block.splitlines()
+    block_type =get_block_from_line_start(block, 1)
+
+    for line_index in range(len(lines)):
+        line = lines[line_index]
+        if get_block_from_line_start(line, line_index + 1) != block_type:
+            return block_type_paragraph
+    return block_type
+
+def get_block_from_line_start(line: str, line_num: int):
+    if line.startswith(">"):
+        return block_type_quote
+    elif line.startswith("- ") or line.startswith("* "):
+        return block_type_unordered_list
+    elif line.startswith(f"{line_num}. "):
+        return block_type_ordered_list
+    else:
+        return block_type_paragraph
--- src/test_markdown.py
+++ src/test_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown import markdown_to_blocks
+from markdown import block_to_block_type, markdown_to_blocks, block_type_code, block_type_quote, block_type_unordered_list, block_type_ordered_list, block_type_paragraph
 
 class TestMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -11,3 +11,37 @@ class TestMarkdown(unittest.TestCase):
             "* list item 1\n* list item 2"
         ]
         self.assertEqual(block_markdown, expected_blocks)
+
+    def test_blocktype_code(self):
+        md = "```js\nconsole.log(\"hello world\");\n```"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, block_type_code)
+
+    def test_blocktype_headers(self):
+        header_text = "This is a header\nAnd I guess this is part of it too?"
+        for i in range(1, 7):
+            header_md = ("#" * i) + header_text
+            header_type = "heading_" + str(i)
+            self.assertEqual(header_type, block_to_block_type(header_md))
+
+    def test_blocktype_quote(self):
+        quote_md = ">Life is like riding a bicycle,\n>To keep your balance,\n>You must keep moving"
+        self.assertEqual(block_type_quote, block_to_block_type(quote_md))
+
+    def test_blocktype_unordered_list(self):
+        unordered_md = "- Item 1\n- Item 2\n- Item 3"
+        self.assertEqual(block_type_unordered_list, block_to_block_type(unordered_md))
+        unordered_md = "* Item 1\n* Item 2\n* Item 3"
+        self.assertEqual(block_type_unordered_list, block_to_block_type(unordered_md))
+
+    def test_blocktype_ordered_list(self):
+        ordered_md = "1. Item 1\n2. Item 2\n3. Item 3"
+        self.assertEqual(block_type_ordered_list, block_to_block_type(ordered_md))
+
+    def test_blocktype_paragraph(self):
+        paragraph_md = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n"
+        self.assertEqual(block_type_paragraph, block_to_block_type(paragraph_md))
+
+        # Test headers above 6 return paragraph
+        paragraph_md = "####### paragraph"
+        self.assertEqual(block_type_paragraph, block_to_block_type(paragraph_md))
